4af6359cba67f34111c70396cfcf2c48a8a28f26
Tushar Bodarya <19bmiit109@gmail.com>
2024-10-26 23:52:58 +0530

    fixes

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(errorHandler);
 
 
 // Starting the server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
